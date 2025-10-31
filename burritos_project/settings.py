e9e526b0c2e662265fd7054a8ca83f862d123574
SandovalRodriguezAngel4-2 <nskjejf@gmail.com>
2025-10-31 09:44:21 -0700

    agregamos una pagina de bienvenida a urls.py

--- burritos_project/settings.py
+++ burritos_project/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'NAME': 'backendsandovalr$burritos_db',
     'USER': 'backendsandovalr',
     'PASSWORD': '12345678',
-    'HOST': 'https://backendsandovalrodriguez.pythonanywhere.com',
+    'HOST': 'backendsandovalr.mysql.pythonanywhere-services.com',
     'PORT': '3307',
     }
 }
